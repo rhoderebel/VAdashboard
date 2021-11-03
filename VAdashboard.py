cda518c88aa1ca4783c7bc3f0367011c042766c6
rhoderebel <91129594+rhoderebel@users.noreply.github.com>
2021-11-03 12:57:26 +0100

    Update VAdashboard.py

--- VAdashboard.py
+++ VAdashboard.py
@@ -16,11 +16,11 @@ geo_co2_merge_2019 = pd.read_csv('geo_co2_merge_2019.csv')
 
 geo_year = [geo_co2_merge_2017, geo_co2_merge_2018, geo_co2_merge_2019]
 
-with open('VAdashboard/geo_co2_merge_2017.json') as geofile:
+with open('geo_co2_merge_2017.json') as geofile:
     geo_co2_merge_2017_json = json.load(geofile) 
-with open('VAdashboard/geo_co2_merge_2018.json') as geofile:
+with open('geo_co2_merge_2018.json') as geofile:
     geo_co2_merge_2017_json = json.load(geofile) 
-with open('VAdashboard/geo_co2_merge_2019.json') as geofile:
+with open('geo_co2_merge_2019.json') as geofile:
     geo_co2_merge_2017_json = json.load(geofile) 
 
 geojson_year = [geo_co2_merge_2017_json, geo_co2_merge_2018_json, geo_co2_merge_2019_json]
