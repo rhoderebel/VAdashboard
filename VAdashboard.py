e083eceba2a8846df96a0d4545cbcedc87df8958
rhoderebel <91129594+rhoderebel@users.noreply.github.com>
2021-11-03 13:40:50 +0100

    Update VAdashboard.py

--- VAdashboard.py
+++ VAdashboard.py
@@ -10,40 +10,7 @@ import numpy as np
 import plotly.graph_objects as go
 import streamlit as st
 
-st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
-
-st.markdown("""
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #3498DB;">
-  <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">Data Professor</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link disabled" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="https://youtube.com/dataprofessor" target="_blank">YouTube</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="https://twitter.com/thedataprof" target="_blank">Twitter</a>
-      </li>
-    </ul>
-  </div>
-</nav>
-""", unsafe_allow_html=True)
-
-# Widget (Cryptocurrency selection box)
-col1_selection = st.sidebar.selectbox()
-col2_selection = st.sidebar.selectbox()
-col3_selection = st.sidebar.selectbox()
-col4_selection = st.sidebar.selectbox()
-col5_selection = st.sidebar.selectbox()
-col6_selection = st.sidebar.selectbox()
-col7_selection = st.sidebar.selectbox()
-col8_selection = st.sidebar.selectbox()
-col9_selection = st.sidebar.selectbox()
+st.sidebar.selectbox('Select a number: ', [1, 2, 3, 4, 5])
 
 geo_co2_merge_2017 = pd.read_csv('geo_co2_merge_2017.csv')
 geo_co2_merge_2018 = pd.read_csv('geo_co2_merge_2018.csv')
