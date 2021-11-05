955de44ec96c523f03d7be072521f88beca3c7cf
rhoderebel <91129594+rhoderebel@users.noreply.github.com>
2021-11-05 09:28:25 +0100

    Update VAdashboard.py

--- VAdashboard.py
+++ VAdashboard.py
@@ -129,10 +129,7 @@ if st.sidebar.selectbox('Kies een pagina: ', ['CO₂-uitstoot', 'Woningdichtheid
         top5_totaal.index = top5_totaal['Gemeenten']
         top5_totaal = top5_totaal[['2017', '2018', '2019']]
         
-        top5_totaal = top5_totaal.style.set_properties(**{
-            'font-size': '10pt',
-        })
-        st.dataframe(top5_totaal)
+        st.table(top5_totaal)
 
     fig = go.Figure()
 
