cc2d0bedea113873ee8a18475ef8600a394d5038
rhoderebel <91129594+rhoderebel@users.noreply.github.com>
2021-11-05 08:48:05 +0100

    Update VAdashboard.py

--- VAdashboard.py
+++ VAdashboard.py
@@ -10,9 +10,9 @@ import numpy as np
 import plotly.graph_objects as go
 import streamlit as st
 
-st.set_page_config(page_title = 'Dashboard CO2-uitstoot en woningdichtheid', layout = 'wide')
+st.set_page_config(page_title = 'Dashboard CO₂-uitstoot en woningdichtheid', layout = 'wide')
 
-if st.sidebar.selectbox('Kies een pagina: ', ['co2 kaarten', 'woningdichtheid kaarten']) == 'co2 kaarten':
+if st.sidebar.selectbox('Kies een pagina: ', ['CO₂-uitstoot', 'Woningdichtheid']) == 'CO₂-uitstoot':
 
     geo_co2_merge_2017 = pd.read_csv('geo_co2_merge_2017.csv')
     geo_co2_merge_2018 = pd.read_csv('geo_co2_merge_2018.csv')
