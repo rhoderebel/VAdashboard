ec0013892df9a9831ae6877fd1eb8f9d45b10613
rhoderebel <91129594+rhoderebel@users.noreply.github.com>
2021-11-05 08:49:26 +0100

    Update VAdashboard.py

--- VAdashboard.py
+++ VAdashboard.py
@@ -140,7 +140,7 @@ if st.sidebar.selectbox('Kies een pagina: ', ['CO₂-uitstoot', 'Woningdichtheid
 
     # Update de layout van de kaart
     fig.update_layout(mapbox_style="carto-positron",
-                      mapbox_zoom=5.8, mapbox_center = {"lat": 52.0893191, "lon": 5.1101691})
+                      mapbox_zoom=5.5, mapbox_center = {"lat": 52.0893191, "lon": 5.1101691})
 
     # Edit slider labels
     fig['layout']['sliders'][0]['currentvalue']['prefix']='Jaar: '
@@ -202,7 +202,7 @@ if st.sidebar.selectbox('Kies een pagina: ', ['CO₂-uitstoot', 'Woningdichtheid
 
     # Update de layout van de kaart
     fig.update_layout(mapbox_style="carto-positron",
-                      mapbox_zoom=5.8, mapbox_center = {"lat": 52.0893191, "lon": 5.1101691})
+                      mapbox_zoom=5.5, mapbox_center = {"lat": 52.0893191, "lon": 5.1101691})
 
     # Edit slider labels
     fig['layout']['sliders'][0]['currentvalue']['prefix']='Jaar: '
