c4e42e70d76cb4180a9ee5697b918f0bcb24571c
rhoderebel <91129594+rhoderebel@users.noreply.github.com>
2021-11-08 13:33:22 +0100

    Update VAdashboard.py

--- VAdashboard.py
+++ VAdashboard.py
@@ -151,8 +151,6 @@ if sidebar_page == 'CO₂-uitstoot':
             # Zet de margins
             fig.update_layout(margin={"r":0, "t": 50, "l": 20, "b": 100},
                               title = '<b>Totaal bekende CO₂-uitstoot (in ton) per gemeente<br>(aardgas, elektr., stadswarmte woningen, voertuigbrandstoffen)</b>',
-                              xref = 'paper',
-                              yref = 'paper',
                               title_x = 0.5,
                               title_y = 0.97,
                               font_family = "sans-serif",
