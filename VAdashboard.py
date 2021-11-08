22a76ab00f370abae36dc5f07e8e497cea18fe2e
rhoderebel <91129594+rhoderebel@users.noreply.github.com>
2021-11-08 13:20:39 +0100

    Update VAdashboard.py

--- VAdashboard.py
+++ VAdashboard.py
@@ -153,7 +153,7 @@ if sidebar_page == 'CO₂-uitstoot':
                               title = '<b>Totaal bekende CO₂-uitstoot (in ton) per gemeente<br>(aardgas, elektr., stadswarmte woningen, voertuigbrandstoffen)</b>',
                               title_x = 0.5,
                               title_y = 0.97,
-                              font_family = "Sans Serif",
+                              font = "Sans Serif",
                               title_font_size = 18)
 
             st.plotly_chart(fig, use_container_width=True) # Laat de plot zien
