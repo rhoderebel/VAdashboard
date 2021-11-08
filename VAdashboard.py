42848d324a33a01fbea49ce1633a125289dede8f
rhoderebel <91129594+rhoderebel@users.noreply.github.com>
2021-11-08 13:55:23 +0100

    Update VAdashboard.py

--- VAdashboard.py
+++ VAdashboard.py
@@ -410,6 +410,9 @@ if sidebar_page == 'CO₂-uitstoot':
                 )
 
                 st.plotly_chart(fig, use_container_width=True)
+        
+            fig = ff.create_distplot(histdata, group_labels = [2017, 2018, 2019])
+            fig.show()
     
     ####################################################################################################################################################################
 
